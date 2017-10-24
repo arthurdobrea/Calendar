ea51f9a4f6c750a94633dd3d94ae3783cb7b4491
kuzeadima <kuzea.k96@gmail.com>
2017-10-24 15:01:06 +0300

    Adding script for PostgreSQL

---
+++ src/main/resources/database_postgres.sql
@@ -0,0 +1,40 @@
+-- auto-generated definition
+CREATE TABLE users
+(
+  id        BIGSERIAL NOT NULL
+    CONSTRAINT users_pkey
+    PRIMARY KEY,
+  username  VARCHAR(255),
+  firstname VARCHAR(255),
+  lastname  VARCHAR(255),
+  email     VARCHAR(255),
+  password  VARCHAR(255)
+);
+
+-- auto-generated definition
+CREATE TABLE roles
+(
+  id   BIGSERIAL NOT NULL
+    CONSTRAINT roles_pkey
+    PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+-- auto-generated definition
+CREATE TABLE user_roles
+(
+  user_id BIGINT
+    CONSTRAINT user_roles_fk
+    REFERENCES users,
+  role_id BIGINT
+    CONSTRAINT user_roles_roles_id_fk
+    REFERENCES roles
+);
+
+INSERT INTO users (username, password) VALUES ('admin', '$2a$11$oBd.94VWAUq6RejwkI4sh.eo7XHOUXpw2oNAMFCLEHpV8fWUligLK');
+--password - 12345678
+
+INSERT INTO roles VALUES (1, 'ROLE_USER');
+INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
+
+INSERT INTO user_roles VALUES (1, 2);
