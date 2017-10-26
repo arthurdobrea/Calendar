8310506e1c660d0fee573cde3d25256c3d815788
isiroshenko <isiroshenko@gmail.com>
2017-10-26 13:10:05 +0300

    first json changes

--- src/main/java/com/calendar/project/model/User.java
+++ src/main/java/com/calendar/project/model/User.java
@@ -43,7 +43,7 @@ public class User {
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles  = new HashSet<>();;
+    private Set<Role> roles  = new HashSet<>();
 
     public Long getId() {
         return id;
