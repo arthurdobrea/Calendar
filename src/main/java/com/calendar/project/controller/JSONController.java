f2f905fda4c55a19ce6d9bab018bd7d066080d9a
isiroshenko <isiroshenko@gmail.com>
2017-10-27 13:22:49 +0300

    add json controller, dependencies, login page - first page, redirect to calendar, some changes in design of calendar

--- src/main/java/com/calendar/project/controller/JSONController.java
+++ src/main/java/com/calendar/project/controller/JSONController.java
@@ -2,23 +2,35 @@ package com.calendar.project.controller;
 
 import com.calendar.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.calendar.project.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 public class JSONController {
 
         @Autowired
         UserService userService;
 
-        @RequestMapping(value="/all", method = RequestMethod.GET)
+//        @RequestMapping(value = "/register", method = RequestMethod.POST,
+//                consumes = MediaType.APPLICATION_JSON_VALUE)
+//        @ResponseStatus(HttpStatus.CREATED)
+//        public void handleJsonPostRequest (@RequestBody User user, Model model) {
+//            System.out.println("saving user: "+ user);
+//            userService.save(user);
+//        }
+
+        @RequestMapping(method = RequestMethod.GET,
+                produces = MediaType.APPLICATION_JSON_VALUE)
+        @ResponseStatus(HttpStatus.OK)
         public @ResponseBody List<User> getUserInJSON() {
             return userService.getAllUsers();
         }
-
 }
