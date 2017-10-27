ed09b4ccf2c968f8506ee533a030c476ba6a2ce0
kuzeadima <kuzea.k96@gmail.com>
2017-10-27 13:08:59 +0300

    Style changes

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,39 +1,40 @@
--- auto-generated definition
-CREATE TABLE users
-(
-  id        BIGSERIAL NOT NULL
-    CONSTRAINT users_pkey
-    PRIMARY KEY,
-  username  VARCHAR(255),
-  firstname VARCHAR(255),
-  lastname  VARCHAR(255),
-  email     VARCHAR(255),
-  password  VARCHAR(255)
-);
+CREATE TABLE users (
+  id       INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+)
 
--- auto-generated definition
-CREATE TABLE roles
-(
-  id   BIGSERIAL NOT NULL
-    CONSTRAINT roles_pkey
-    PRIMARY KEY,
-  name VARCHAR(255)
-);
+ ENGINE = InnoDB;
 
--- auto-generated definition
-CREATE TABLE user_roles
-(
-  user_id BIGINT
-    CONSTRAINT user_roles_fk
-    REFERENCES users(id),
-  role_id BIGINT
-    CONSTRAINT user_roles_roles_id_fk
-    REFERENCES roles(id)
-);
+CREATE TABLE  roles(
+  id int NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+)
+  ENGINE = InnoDB;
 
-INSERT INTO users (username, password) VALUES ('admin', '$2a$11$oBd.94VWAUq6RejwkI4sh.eo7XHOUXpw2oNAMFCLEHpV8fWUligLK');
+CREATE TABLE  user_roles(
+  user_id int not NULL,
+  role_id int not null,
 
-INSERT INTO roles VALUES (1, 'ROLE_USER');
-INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
 
-INSERT INTO user_roles VALUES (1, 2);
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+
+  UNIQUE (user_id,role_id)
+)
+
+  ENGINE = InnoDB;
+
+INSERT INTO users VALUES (1, 'admin','312r821747f19f1830ue984f57910fj');
+
+INSERT roles VALUES (1,'ROLE_USER');
+INSERT roles VALUES (2,'ROLE_ADMIN');
+
+INSERT INTO user_roles VALUES(1,2);
+
+--Inserts for testing
+-- if there are some problems with inserts - correct them
+INSERT INTO users
+    VALUES (99, 'UserTest', 'Qawsed', 'Qawsed', 'adamaa14@gmail.com', '$2a$11$TDrIdfhId/ON7V0han8Fa.tS7eBdJ6LooYNQPnBU8CM3Jgcf7q2UG');
+
+INSERT INTO user_roles VALUES(99,1);
