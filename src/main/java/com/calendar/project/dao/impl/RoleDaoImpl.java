fca12717eec9d334a3234129fa4f116bbb401ec1
eugenciolacu <eugenciolacu@gmail.com>
2017-11-17 14:00:51 +0200

    Added logs for RoleDaoImpl and TagDaoImpl

--- src/main/java/com/calendar/project/dao/impl/RoleDaoImpl.java
+++ src/main/java/com/calendar/project/dao/impl/RoleDaoImpl.java
@@ -19,17 +19,20 @@ public class RoleDaoImpl implements RoleDao {
 
     @Override
     public Role getRole(Long id) {
+        LOGGER.info("Return role with id = " + id);
         return entityManager.find(Role.class, id);
     }
 
     @Override
     public Role findById(Long id) {
+        LOGGER.info("Return role with id = " + id);
         return entityManager.createQuery("from Role r where r.id: = id", Role.class)
                 .getSingleResult();
     }
 
     @Override
     public Long findRoleIdByValue(String roleValue) {
+        LOGGER.info("Return id of a role = " + roleValue);
         return entityManager.createQuery("select r.id from Role r where r.name = :roleValue", Long.class)
                 .setParameter("roleValue", roleValue)
                 .getSingleResult();
@@ -37,6 +40,7 @@ public class RoleDaoImpl implements RoleDao {
 
     @Override
     public List<Role> findAll() {
+        LOGGER.info("Return a list with all roles");
         return entityManager.createQuery("from Role r", Role.class)
                 .getResultList();
     }
